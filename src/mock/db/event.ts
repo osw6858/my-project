fcb59bef2ea4fd64023e3259289724ea3a81567d
osw6858 <osw7875@naver.com>
2024-12-19 18:47:57 +0900

    chore: MSW 목데이터 일부 수정

--- src/mock/db/event.ts
+++ src/mock/db/event.ts
@@ -1,8 +1,20 @@
 export const DB = {
   event: [
-    { id: '1', title: '일정', startDate: '2024-12-10', endDate: '2024-12-14' },
+    {
+      id: '1',
+      title: '일정',
+      content: '일본 여행',
+      startDate: '2024-12-10',
+      endDate: '2024-12-14',
+    },
     { id: '2', title: '휴가', startDate: '2024-12-20', endDate: '2024-12-22' },
-    { id: '3', title: '회의', startDate: '2024-12-22', endDate: '2024-12-22' },
+    {
+      id: '3',
+      title: '회의',
+      content: '프로젝트 회의',
+      startDate: '2024-12-22',
+      endDate: '2024-12-22',
+    },
     { id: '4', title: '회의2', startDate: '2024-11-22', endDate: '2024-11-22' },
   ],
-} as const
+}

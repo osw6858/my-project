6c4988a0323113a81d83503ebd26bd8cd495380e
osw6858 <osw7875@naver.com>
2024-12-19 12:23:08 +0900

    design: 해더 푸터 UI 및 CSS 수정

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="w-full h-14">
+    <footer className="w-full h-14 border-t border-solid border-gray-200">
       <div className="container mx-auto flex justify-center items-center h-full">
         <span>copy right</span>
       </div>
--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,8 +1,16 @@
+import Link from 'next/link'
+
 export default function Header() {
   return (
-    <header className="w-full">
-      <div className="container mx-auto flex items-center h-14">
-        <h1>logo</h1>
+    <header className="w-full border-b border-solid border-gray-200">
+      <div className="container mx-auto flex items-center h-14 gap-10">
+        <Link href="/">
+          <h1>logo</h1>
+        </Link>
+        <nav className="space-x-5">
+          <Link href="/calendar">캘린더</Link>
+          <Link href="/">채팅</Link>
+        </nav>
       </div>
     </header>
   )

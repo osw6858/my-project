c28195f69d796a643c5286742a140a3bd5e4fb26
osw6858 <osw7875@naver.com>
2024-12-23 17:07:12 +0900

    fix: 불필요한 React.memo 비교 함수 제거

--- src/app/(home)/chat/_components/Messages.tsx
+++ src/app/(home)/chat/_components/Messages.tsx
@@ -8,38 +8,33 @@ interface MessageProps {
   message: Message
 }
 
-const Messages = React.memo(
-  ({ message }: MessageProps) => {
-    console.log('리렌더링')
-    if (message.type === 'card') {
-      return (
-        <CardMessage
-          title={message.content}
-          introduce={message.introduce}
-          url={message.url}
-          tel={message.tel}
-          fromSelf={message.fromSelf}
-        />
-      )
-    }
+const Messages = React.memo(({ message }: MessageProps) => {
+  console.log('리렌더링')
+  if (message.type === 'card') {
+    return (
+      <CardMessage
+        title={message.content}
+        introduce={message.introduce}
+        url={message.url}
+        tel={message.tel}
+        fromSelf={message.fromSelf}
+      />
+    )
+  }
 
-    if (message.type === 'file' && message.file) {
-      return (
-        <FileMessage
-          fileName={message.file.fileName}
-          fileType={message.file.fileType}
-          fileData={message.file.fileData}
-          fromSelf={message.fromSelf}
-        />
-      )
-    }
+  if (message.type === 'file' && message.file) {
+    return (
+      <FileMessage
+        fileName={message.file.fileName}
+        fileType={message.file.fileType}
+        fileData={message.file.fileData}
+        fromSelf={message.fromSelf}
+      />
+    )
+  }
 
-    return <TextMessage message={message} />
-  },
-  (prevProps, nextProps) => {
-    return prevProps.message === nextProps.message
-  },
-)
+  return <TextMessage message={message} />
+})
 
 Messages.displayName = 'Messages'
 
